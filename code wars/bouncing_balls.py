fdf956ba47835b153a11dd269e90dd6e8582797a
nivesh <nivesh.nk1000@gmail.com>
2024-04-08 14:30:00 +0530

    one solution added to github

---
+++ code wars/bouncing_balls.py
@@ -0,0 +1,14 @@
+def bouncing_ball(h, bounce, window):
+    c=0
+    if(bounce>0 and bounce<1 and h>window and h>0):
+        if(h>window):
+            c+=1
+        while(h>window):
+            h*=bounce
+            if(h>window):
+                c+=2
+        print(c)        
+    else:
+        print(-1) 
+bouncing_ball(3,1, 1.5)
+    
