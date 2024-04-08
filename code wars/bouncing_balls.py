0a8f78c364f9c4292acac67e452852f706220fe1
nivesh <nivesh.nk1000@gmail.com>
2024-04-08 14:35:17 +0530

    change:question also added along with answer

--- code wars/bouncing_balls.py
+++ code wars/bouncing_balls.py
@@ -1,3 +1,4 @@
+#question link :https://www.codewars.com/kata/5544c7a5cb454edb3c000047/python
 def bouncing_ball(h, bounce, window):
     c=0
     if(bounce>0 and bounce<1 and h>window and h>0):
@@ -7,8 +8,7 @@ def bouncing_ball(h, bounce, window):
             h*=bounce
             if(h>window):
                 c+=2
-        print(c)        
+        print(c)
     else:
-        print(-1) 
+        print(-1)
 bouncing_ball(3,1, 1.5)
-    
